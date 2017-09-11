961e3362ffc1f52c363c5580614cbe291bd16c7a
pepese <tanakakns@tanakaknsnoMacBook-Pro.local>
2017-09-11 18:06:04 +0900

    update

--- pn_dic.js
+++ pn_dic.js
@@ -3,8 +3,6 @@ const readline = require('readline');
 const rs = fs.createReadStream('./dic/pn_ja.dic');
 const rl = readline.createInterface({'input': rs, 'output': {}});
 
-const async = require('async');
-
 let noun = [];
 let verb = [];
 let adjective = [];
@@ -13,13 +11,10 @@ let auxiliary = [];
 let setUpEndFlag = false;
 
 const setUp = () => {
-  return new Promise(() => {
-    let i = 0;
+  return new Promise((resolve, reject) => {
     if(!setUpEndFlag) {
       rl.on('line', (line) => {
         const word = line.split(":");
-        i++;
-        //console.log("word[0]: " + word[0] + ", word[2]: " + word[2] + " / " + i);
         if(word[2] == "名詞") {
           noun.push(word);
         } else if(word[2] == "動詞") {
@@ -30,71 +25,43 @@ const setUp = () => {
           adverb.push(word);
         } else if(word[2] == "助動詞") {
           auxiliary.push(word);
-        } else {
-          console.log(word[2] + " / " + i);
         }
-      }).on('pause', () => {
-        console.log("Pause.");
-        rl.resume();
-      }).on('resume', () => {
-        console.log("Resume.");
-      }).on('SIGCONT', () => {
-        console.log("SIGCONT.");
-      }).on('SIGINT', () => {
-        console.log("SIGINT.");
-      }).on('SIGTSTP', () => {
-        console.log("SIGTSTP.");
       }).on('close', () => {
-        console.log("End.");
         setUpEndFlag = true;
+        resolve();
       });
     }
-    return;
   });
 };
-// setUp().then(() => {
-//   console.log(adjective);
-//   setUpEndFlag = true;
-// });
 
 const match = (word, part) => {
   for(let i=0; i<part.length; i++) {
     const dic_word = part[i][0];
     const dic_score = parseFloat(part[i][3]);
-    console.log("roop2: " + dic_word);
     if(dic_word == word) {
       return dic_score;
     }
   }
+  return 0;
 }
 
 const calc = (morpheme_arr) => {
   return setUp().then(() => {
-    console.log("adjective: " + adjective);
-  }).then(() => {
     let score = 0;
     for(let i=0; i<morpheme_arr.length; i++) {
-      console.log("roop1");
       const morpheme = morpheme_arr[i];
       const part = morpheme[1];
       const word = morpheme[7];
       if(part == "名詞") {
-        console.log("名詞: " + word);
         score = score + match(word, noun);
       } else if(part == "動詞") {
-        console.log("動詞: " + word);
         score = score + match(word, verb);
       } else if(part == "形容詞") {
-        console.log("形容詞: " + word);
         score = score + match(word, adjective);
       } else if(part == "副詞") {
-        console.log("副詞: " +  + word);
         score = score + match(word, adverb);
       } else if(part == "助動詞") {
-        console.log("助動詞: " + word);
         score = score + match(word, auxiliary);
-      } else {
-        return 0;
       }
     }
     return score;
